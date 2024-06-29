9689139df1aaebc556f703a2cb43d0e8a07a3038
fyerfyer <fuy60703@gmail.com>
2024-06-29 19:56:06 +0800

    completed lab08

--- lab/lab08/simd.c
+++ lab/lab08/simd.c
@@ -51,10 +51,23 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
     long long int result = 0; // This is where you should put your final result!
     /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
 
+    int tmp[4];
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* YOUR CODE GOES HERE */
+        for (unsigned int i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+            __m128i comparator = _mm_loadu_si128((__m128i *)&vals[i]);
+            __m128i mask = _mm_cmpgt_epi32(comparator, _127);
+            __m128i compare_result = _mm_and_si128(mask, comparator);
+            
 
+            _mm_storeu_si128((__m128i *)tmp, compare_result);
+            result += tmp[0] + tmp[1] + tmp[2] + tmp[3];
+        }
         /* Hint: you'll need a tail case. */
+        for (unsigned int i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i ++) {
+            if (vals[i] >= 128) 
+                result += vals[i];
+        }
     }
 
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
@@ -69,11 +82,40 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
     long long int result = 0;
     /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
 
+    int tmp[4];
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* YOUR CODE GOES HERE */
         /* Copy your sum_simd() implementation here, and unroll it */
+        for (unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16) {
+            __m128i comparator = _mm_loadu_si128((__m128i *)&vals[i]);
+            __m128i mask = _mm_cmpgt_epi32(comparator, _127);
+            __m128i compare_result = _mm_and_si128(mask, comparator);
+            _mm_storeu_si128((__m128i *)tmp, compare_result);
+            result += tmp[0] + tmp[1] + tmp[2] + tmp[3];
 
+            comparator = _mm_loadu_si128((__m128i *)&vals[i + 4]);
+            mask = _mm_cmpgt_epi32(comparator, _127);
+            compare_result = _mm_and_si128(mask, comparator);
+            _mm_storeu_si128((__m128i *)tmp, compare_result);
+            result += tmp[0] + tmp[1] + tmp[2] + tmp[3];
+
+            comparator = _mm_loadu_si128((__m128i *)&vals[i + 8]);
+            mask = _mm_cmpgt_epi32(comparator, _127);
+            compare_result = _mm_and_si128(mask, comparator);
+            _mm_storeu_si128((__m128i *)tmp, compare_result);
+            result += tmp[0] + tmp[1] + tmp[2] + tmp[3];
+
+            comparator = _mm_loadu_si128((__m128i *)&vals[i + 12]);
+            mask = _mm_cmpgt_epi32(comparator, _127);
+            compare_result = _mm_and_si128(mask, comparator);
+            _mm_storeu_si128((__m128i *)tmp, compare_result);
+            result += tmp[0] + tmp[1] + tmp[2] + tmp[3];
+        }
         /* Hint: you'll need 1 or maybe 2 tail cases here. */
+        for (unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS; i += 1) {
+            if (vals[i] >= 128)
+                result += vals[i];
+        }
     }
 
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
